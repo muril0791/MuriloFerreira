5aa18404f08fb8d23d28af4ae221bdea0e88f41c
Murilo Ferreira dos Santos <murilo@jogoglobal.com>
2023-10-25 02:15:23 -0300

    Fix more

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,10 +1,16 @@
-// https://nuxt.com/docs/api/configuration/nuxt-config
+
+const path = require('path')
 export default defineNuxtConfig({
   ssr: false,
   css: [
     "vuetify/lib/styles/main.sass",
     "@mdi/font/css/materialdesignicons.min.css",
   ],
+  nitro: {
+    output: {
+      publicDir: path.join(__dirname, 'dist')
+    }
+  },
   build: {
     transpile: ["vuetify"],
   },
